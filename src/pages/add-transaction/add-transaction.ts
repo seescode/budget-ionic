0254bd5f81963a806bf3c433b7a13272498bba7a
seescode <seesyong@gmail.com>
2017-09-24 20:43:28 -0400

    Get the add transaction pages setup

--- src/pages/add-transaction/add-transaction.ts
+++ src/pages/add-transaction/add-transaction.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { ViewController } from 'ionic-angular';
-import { IonicPage } from 'ionic-angular';
+import { IonicPage, NavParams } from 'ionic-angular';
 
 @IonicPage()
 @Component({
@@ -9,14 +9,53 @@ import { IonicPage } from 'ionic-angular';
 })
 export class AddTransactionPage {
 
+  // TODO: move this into the store as a read only data source
+  categoryMap = {
+    'education': [
+      'Classes',
+      'College Savings'
+    ],
+    'food': [
+      'Groceries',
+      'Dining Out',
+      'Alcohol'
+    ],
+    'fun': [
+      'Entertainment',
+      'Gifts'
+    ],
+    'home': [
+      'Rent/Mortgage'
+    ],
+    'medical': [
+      'Medical',
+      'Dental'
+    ],
+    'stuff': [
+      'Supplies',
+      'Clothes',
+      'Software Subscriptions'
+    ],
+    'transportation': [
+      'Public Transportation',
+      'Car',
+      'Plane'
+    ],
+    'utilities': [
+      'Electric',
+      'Gas',
+      'Laundry',
+      'Phone',
+      'Internet'
+    ]
+  };
 
-  categories = [
-    'Public Transportation',
-    'Car',
-    'Plane'
-  ];
+  categories = [];
 
-  constructor(public viewCtrl: ViewController) {
+  constructor(public viewCtrl: ViewController, private navParams: NavParams) {
+    let id = navParams.get('category');
+    
+    this.categories = this.categoryMap[id];
   }
 
   cancel() {
--- src/pages/budget/budget.ts
+++ src/pages/budget/budget.ts
@@ -8,12 +8,48 @@ import { NavController, ModalController, IonicPage } from 'ionic-angular';
 })
 export class BudgetPage {
 
+  // TODO: Should be coming from the store.
+  categories = [
+    {
+      icon: 'pizza',
+      name: 'Food'
+    },
+    {
+      icon: 'pricetag',
+      name: 'Stuff'
+    },
+    {
+      icon: 'car',
+      name: 'Transportation'
+    },
+    {
+      icon: 'outlet',
+      name: 'Utilities'
+    },
+    {
+      icon: 'home',
+      name: 'Home'
+    },
+    {
+      icon: 'book',
+      name: 'Education'
+    },
+    {
+      icon: 'happy',
+      name: 'Fun'
+    },
+    {
+      icon: 'heart',
+      name: 'Medical'
+    }
+  ];
+
   constructor(public navCtrl: NavController, public modalCtrl: ModalController) {
 
   }
 
-  addTransaction() {
-    let modal = this.modalCtrl.create('AddTransactionPage');
+  addTransaction(category: string) {
+    let modal = this.modalCtrl.create('AddTransactionPage', { category: category.toLowerCase() });
     modal.present();    
   }
 
